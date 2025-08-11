4784e806137259faf7092cf8d5aeee0b4763a318
Bishal Shrestha <bishal.shrestha@outlook.com.au>
2025-08-11 09:13:55 +0000

    Fix trailing whitespace and add missing final newline in Tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -16,7 +16,7 @@ class Tag < ApplicationRecord
       )
     )
   }
-  
+
   COLORS = %w[#e99537 #4da568 #6471eb #db5a54 #df4e92 #c44fe9 #eb5429 #61c9ea #805dee #6ad28a]
 
   UNCATEGORIZED_COLOR = "#737373"
