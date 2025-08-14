8dd3c5aeeae8676c6a1aee746fa7d5fa72bcf07e
Amr Awad <amr@islamicfinanceguru.com>
2025-08-14 08:26:18 +0100

    update breadcrumbs on Lunch Flow page

--- app/controllers/settings/lunch_flow_controller.rb
+++ app/controllers/settings/lunch_flow_controller.rb
@@ -2,6 +2,11 @@ class Settings::LunchFlowController < ApplicationController
   layout "settings"
 
   def show
+    @breadcrumbs = [
+      [ "Home", root_path ],
+      [ "Bank Sync", settings_bank_sync_path ],
+      [ "Lunch Flow", nil ]
+    ]
   end
 end
 
