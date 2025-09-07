e51ff58522bbf7dd3ca893091908db6e9e7c4762
Alessio Cappa <alessiocappa26@gmail.com>
2025-09-07 12:29:49 +0200

    refactor: move creation/update logic for Enable Banking items in connectable

--- app/controllers/enable_banking_items_controller.rb
+++ app/controllers/enable_banking_items_controller.rb
@@ -52,18 +52,12 @@ class EnableBankingItemsController < ApplicationController
       redirect_to enable_banking_items_path, alert: t(".auth_failed")
     else
       enable_banking_id = params[:state]
-      @enable_banking_item = Current.family.enable_banking_items.find_or_create_by(id: enable_banking_id)
-      session = enable_banking_provider.create_session(code)
-      @enable_banking_item.update!(
-        session_id: session["session_id"],
-        valid_until: session["access"]["valid_until"],
-        status: "good",
-        aspsp_name: session["aspsp"]["name"],
-        aspsp_country: session["aspsp"]["country"],
-        logo_url: "https://enablebanking.com/brands/#{session['aspsp']['country']}/#{session['aspsp']['name']}",
-        raw_payload: session.to_json
+      Current.family.create_enable_banking_item!(
+        enable_banking_id: enable_banking_id,
+        session_id: code
       )
-      redirect_to enable_banking_items_path, notice: t(".success")
+
+      redirect_to accounts_path, notice: t(".success")
     end
   end
 
