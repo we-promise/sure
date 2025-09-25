755dbb678fd00686454096a9f71695e0f4664987
Juan José Mata <juanjo.mata@gmail.com>
2025-09-25 11:37:39 +0000

    No longer Sure, configurable

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: email_address_with_name(
     ENV.fetch("EMAIL_SENDER", "sender@sure.local"),
-    "#{Rails.configuration.x.product_name} Finance"
+    "#{Rails.configuration.x.brand_name} #{Rails.configuration.x.product_name}"
   )
   layout "mailer"
 
