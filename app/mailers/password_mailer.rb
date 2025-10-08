e6c0c752bc9a4dadc83b37046e05f721c6fa5308
Juan José Mata <juanjo.mata@gmail.com>
2025-10-08 10:13:14 +0000

    Fix i18n errors/tests

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,7 +1,7 @@
 class PasswordMailer < ApplicationMailer
   def password_reset
     @user = params[:user]
-    @subject = t(".subject", product: product_name)
+    @subject = t(".subject", product_name: product_name)
     @cta = t(".cta")
 
     mail to: @user.email, subject: @subject
