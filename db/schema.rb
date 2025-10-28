38584603d5ac13a484390c66200d719442b1d714
sokie <sokysrm@gmail.com>
2025-10-28 11:45:08 +0100

    Fix account schema for multipe providers
      Can do:
      - Account #1 → PlaidAccount + SimplefinAccount (multiple different providers)
      - Account #2 → PlaidAccount only
      - Account #3 → SimplefinAccount only
    
      Cannot do:
      - Account #1 → PlaidAccount + PlaidAccount (duplicate provider type)
      - PlaidAccount #123 → Account #1 + Account #2 (provider linked to multiple accounts)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_27_141735) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_28_104241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -25,8 +25,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_27_141735) do
     t.uuid "provider_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["account_id"], name: "index_account_providers_on_account_id", unique: true
-    t.index ["provider_type", "provider_id"], name: "index_account_providers_on_provider"
+    t.index ["account_id", "provider_type"], name: "index_account_providers_on_account_and_provider_type", unique: true
     t.index ["provider_type", "provider_id"], name: "index_account_providers_on_provider_type_and_provider_id", unique: true
   end
 
