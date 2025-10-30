b70782281c6d79c065e3de08b1f0d7cba4c699c4
Juan José Mata <juanjo.mata@gmail.com>
2025-10-30 12:50:45 +0000

    Remove per conversation with @sokie

--- db/migrate/20251029204447_remove_lunchflow_account_id_from_accounts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLunchflowAccountIdFromAccounts < ActiveRecord::Migration[7.2]
-  def change
-    remove_reference :accounts, :lunchflow_account, foreign_key: true, type: :uuid
-  end
-end
