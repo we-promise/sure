fcecafa29fe5b714633d12cdb334be4b0c118d6c
Juan José Mata <juanjo.mata@gmail.com>
2025-12-06 13:40:45 +0000

    Fix AR migration version

--- db/migrate/20251118000000_add_rule_fields_to_import_rows.rb
+++ db/migrate/20251118000000_add_rule_fields_to_import_rows.rb
@@ -1,4 +1,4 @@
-class AddRuleFieldsToImportRows < ActiveRecord::Migration[8.0]
+class AddRuleFieldsToImportRows < ActiveRecord::Migration[7.2]
   def change
     add_column :import_rows, :resource_type, :string
     add_column :import_rows, :active, :boolean
