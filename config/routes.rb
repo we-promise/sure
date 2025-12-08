97620e9c667b045b92134e20e69e9f7966c1d20d
Alessio Cappa <alessiocappa26@gmail.com>
2025-12-08 21:44:35 +0100

    feat: Allow account linking for Enable Banking accounts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     collection do
       get :callback
       post :link_accounts
+      get :select_existing_account
+      post :link_existing_account
     end
     member do
       post :sync
