4a39f659877aa3e03ebafaf0ce7266062a9fa71b
Alessio Cappa <alessiocappa26@gmail.com>
2025-12-13 13:43:21 +0100

    feat: Add toggle on mobile to show/hide checkboxes in transaction page

---
+++ app/javascript/controllers/checkbox_toggle_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["selectionEntry", "toggleButton"]
+
+  toggle() {
+    const shouldShow = this.selectionEntryTargets[0].classList.contains("hidden")
+
+    this.selectionEntryTargets.forEach((el) => {
+      if (shouldShow) {
+        el.classList.remove("hidden")
+      } else {
+        el.classList.add("hidden")
+      }
+    })
+
+    if (!shouldShow) {
+      const bulkSelectElement = this.element.closest("[data-controller~='bulk-select']");
+      if (bulkSelectElement) {
+        const bulkSelectController = this.application.getControllerForElementAndIdentifier(
+          bulkSelectElement,
+          "bulk-select"
+        );
+        if (bulkSelectController) {
+          bulkSelectController.deselectAll();
+        }
+      }
+    }
+
+    this.toggleButtonTarget.classList.toggle("bg-surface", shouldShow)
+  }
+}
