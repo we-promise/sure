20944714beacd9001d18cbf4c58ba04c2f53c0eb
Carlos Adames <cj@Carloss-MacBook-Air.local>
2025-12-25 13:30:47 -0400

    Fix trailing whitespace in DragAndDropImportTest

--- test/system/drag_and_drop_import_test.rb
+++ test/system/drag_and_drop_import_test.rb
@@ -9,13 +9,13 @@ class DragAndDropImportTest < ApplicationSystemTestCase
     visit transactions_path
 
     assert_selector "#transactions[data-controller*='drag-and-drop-import']"
-    
+
     # We can't easily simulate a true native drag-and-drop in headless chrome via Capybara without complex JS.
     # However, we can verify that the hidden form exists and works when a file is "dropped" (input populated).
     # The Stimulus controller's job is just to transfer the dropped file to the input and submit.
-    
+
     file_path = file_fixture("imports/transactions.csv")
-    
+
     # Manually make form and input visible
     execute_script("
       var form = document.querySelector('form[action=\"#{imports_path}\"]');
@@ -24,9 +24,9 @@ class DragAndDropImportTest < ApplicationSystemTestCase
       input.classList.remove('hidden');
       input.style.display = 'block';
     ")
-    
+
     attach_file "import[csv_file]", file_path
-    
+
     # Submit the form manually since we bypassed the 'drop' event listener which triggers submit
     find("form[action='#{imports_path}']").evaluate_script("this.requestSubmit()")
 
