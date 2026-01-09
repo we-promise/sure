49b0bbe3104bccff519f0404f316b0786f6d4ded
sokie <sokysrm@gmail.com>
2026-01-09 15:04:56 +0100

    FIX logos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_09_100000) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_09_135841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
