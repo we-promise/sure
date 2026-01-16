dcdb071e8dc86ad7b40c00f01448e74172ae5f3e
Jose <39016041+jospaquim@users.noreply.github.com>
2026-01-16 09:57:44 -0500

    Enhance merchants controller tests for family scope
    Added tests to ensure that merchants from other families are not returned in the index action and that accessing a merchant from another family returns a 404 error.
    
    Signed-off-by: Jose <39016041+jospaquim@users.noreply.github.com>

--- test/controllers/api/v1/merchants_controller_test.rb
+++ test/controllers/api/v1/merchants_controller_test.rb
@@ -5,6 +5,7 @@ require "test_helper"
 class Api::V1::MerchantsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:family_admin)
+    @other_family_user = users(:empty)
 
     @oauth_app = Doorkeeper::Application.create!(
       name: "Test App",
@@ -37,14 +38,27 @@ class Api::V1::MerchantsControllerTest < ActionDispatch::IntegrationTest
 
     merchants = JSON.parse(response.body)
     assert_kind_of Array, merchants
+    assert_not_empty merchants
 
-    if merchants.any?
-      merchant = merchants.first
-      assert merchant.key?("id")
-      assert merchant.key?("name")
-      assert merchant.key?("created_at")
-      assert merchant.key?("updated_at")
-    end
+    merchant = merchants.first
+    assert merchant.key?("id")
+    assert merchant.key?("name")
+    assert merchant.key?("created_at")
+    assert merchant.key?("updated_at")
+  end
+
+  test "index does not return merchants from other families" do
+    # Create a merchant in another family
+    other_merchant = @other_family_user.family.merchants.create!(name: "Other Merchant")
+
+    get api_v1_merchants_url, headers: auth_headers
+
+    assert_response :success
+    merchants = JSON.parse(response.body)
+    merchant_ids = merchants.map { |m| m["id"] }
+
+    assert_includes merchant_ids, @merchant.id
+    assert_not_includes merchant_ids, other_merchant.id
   end
 
   # Show action tests
@@ -66,6 +80,14 @@ class Api::V1::MerchantsControllerTest < ActionDispatch::IntegrationTest
 
   test "show returns 404 for non-existent merchant" do
     get api_v1_merchant_url(id: SecureRandom.uuid), headers: auth_headers
+
+    assert_response :not_found
+  end
+
+  test "show returns 404 for merchant from another family" do
+    other_merchant = @other_family_user.family.merchants.create!(name: "Other Merchant")
+
+    get api_v1_merchant_url(other_merchant), headers: auth_headers
 
     assert_response :not_found
   end
