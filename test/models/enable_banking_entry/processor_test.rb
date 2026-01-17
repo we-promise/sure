19c3306bc1c0cc072edb4f03bd17ce08fcfec870
quentinreytinas <quentin@reytinas.fr>
2026-01-17 14:45:19 +0000

    EnableBanking: skip CARD-* counterparty in name
    # Conflicts:
    #	test/models/enable_banking_entry/processor_test.rb
    
    # Conflicts:
    #	test/models/enable_banking_entry/processor_test.rb

--- test/models/enable_banking_entry/processor_test.rb
+++ test/models/enable_banking_entry/processor_test.rb
@@ -208,4 +208,31 @@ class EnableBankingEntry::ProcessorTest < ActiveSupport::TestCase
     entry = @account.entries.find_by!(external_id: "enable_banking_ref_noextra")
     assert_nil entry.transaction&.extra&.dig("enable_banking")
   end
+
+  def build_name(data)
+    processor = EnableBankingEntry::Processor.new(data, enable_banking_account: Object.new)
+    processor.send(:name)
+  end
+
+  test "skips technical card counterparty and falls back to bank tx description" do
+    name = build_name(
+      credit_debit_indicator: "CRDT",
+      debtor_name: "CARD-1234",
+      remittance_information: ["ACME SHOP"],
+      bank_transaction_code: { description: "Card Purchase" }
+    )
+
+    assert_equal "Card Purchase", name
+  end
+
+  test "uses counterparty when it is human readable" do
+    name = build_name(
+      credit_debit_indicator: "CRDT",
+      debtor_name: "ACME SHOP",
+      remittance_information: ["Receipt #42"],
+      bank_transaction_code: { description: "Transfer" }
+    )
+
+    assert_equal "ACME SHOP", name
+  end
 end
