eab55da51f55ef629650e63247ce2899dca62e18
Juan José Mata <jjmata@jjmata.com>
2026-01-19 18:02:53 +0100

    Change default per_page value from 20 to 10
    Signed-off-by: Juan José Mata <jjmata@jjmata.com>

--- app/controllers/concerns/safe_pagination.rb
+++ app/controllers/concerns/safe_pagination.rb
@@ -4,7 +4,7 @@ module SafePagination
   extend ActiveSupport::Concern
 
   private
-    def safe_per_page(default = 20)
+    def safe_per_page(default = 10)
       allowed_values = [ 10, 20, 50, 100 ]
       per_page = params[:per_page].to_i
 
