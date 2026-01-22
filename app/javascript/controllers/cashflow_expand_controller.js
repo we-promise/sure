696db6c33c76b026a8c589c628594aa41fc11f05
Number <55629655+CylonN8@users.noreply.github.com>
2026-01-22 21:17:57 +0100

    refactor: declarative draggable restore on cashflow dialog close

--- app/javascript/controllers/cashflow_expand_controller.js
+++ app/javascript/controllers/cashflow_expand_controller.js
@@ -3,15 +3,25 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   open() {
     const dialog = this.element.querySelector("dialog");
+    if (!dialog) return;
+
+    // Capture the current draggable state only if it hasn't been captured yet
+    if (this.originalDraggable === undefined) {
+      this.originalDraggable = this.element.getAttribute("draggable");
+    }
 
     // Disable dragging on the section while dialog is open
     this.element.setAttribute("draggable", "false");
 
-    // Re-enable dragging when dialog closes
-    dialog?.addEventListener('close', () => {
-      this.element.setAttribute("draggable", "true");
-    }, { once: true });
+    dialog.showModal();
+  }
 
-    dialog?.showModal();
+  restore() {
+    if (this.originalDraggable) {
+      this.element.setAttribute("draggable", this.originalDraggable);
+    } else {
+      this.element.removeAttribute("draggable");
+    }
+    this.originalDraggable = undefined;
   }
 }
