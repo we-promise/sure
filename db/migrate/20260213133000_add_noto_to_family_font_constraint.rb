b34debb67fe5d5b4ed4ad525ba4659050ca894ba
dion <1079894021@qq.com>
2026-02-14 19:12:40 +0800

    delete some useless files

--- db/migrate/20260213133000_add_noto_to_family_font_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddNotoToFamilyFontConstraint < ActiveRecord::Migration[7.2]
-  def change
-    remove_check_constraint :families, name: "families_font_allowed_values"
-    add_check_constraint :families, "font IN ('sans', 'display', 'mono', 'noto')", name: "families_font_allowed_values"
-  end
-end
