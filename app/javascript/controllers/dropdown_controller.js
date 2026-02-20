7c1a1b27390323c99c9e7d7be7a7964825682861
Alessio Cappa <alessiocappa26@gmail.com>
2026-02-20 23:50:40 +0100

    feat: improve dropdown controller

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,10 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "button"]
-  
-  toggle() {
-    this.menuTarget.classList.toggle("hidden")
+  static targets = ["button", "menu"]
+  static values = { open: Boolean }
+
+  connect() {
+    this.open = false
+    this.boundOutsideClick = this.handleOutsideClick.bind(this)
+    this.boundKeydown = this.handleKeydown.bind(this)
+
+    document.addEventListener("click", this.boundOutsideClick)
+    this.element.addEventListener("keydown", this.boundKeydown)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.boundOutsideClick)
+    this.element.removeEventListener("keydown", this.boundKeydown)
+  }
+
+  toggle = () => {
+    this.open ? this.close() : this.openMenu()
+  }
+
+  openMenu() {
+    this.open = true
+    this.menuTarget.classList.remove("hidden")
+    this.focusSearch()
+  }
+
+  close() {
+    this.open = false
+    this.menuTarget.classList.add("hidden")
   }
 
   select(event) {
@@ -13,17 +39,38 @@ export default class extends Controller {
 
     this.buttonTarget.textContent = label
 
-    const input = this.element.querySelector("input[type=hidden]")
-    if (input) {
-      input.value = value
-      input.dispatchEvent(new Event("change", { bubbles: true }))
+    this.element.dispatchEvent(
+      new CustomEvent("dropdown:select", {
+        detail: { value, label },
+        bubbles: true
+      })
+    )
+
+    this.close()
+  }
+
+  focusSearch() {
+    const input = this.menuTarget.querySelector('input[type="search"]')
+    if (input) input.focus({ preventScroll: true })
+  }
+
+  handleOutsideClick(event) {
+    if (this.open && !this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+
+  handleKeydown(event) {
+    if (!this.open) return
+
+    if (event.key === "Escape") {
+      this.close()
+      this.buttonTarget.focus()
     }
 
-    this.menuTarget.classList.add("hidden")
-
-    this.element.dispatchEvent(new CustomEvent("dropdown:select", {
-      detail: { value: value, label: label },
-      bubbles: true
-    }))
+    if (event.key === "Enter" && event.target.dataset.value) {
+      event.preventDefault()
+      event.target.click()
+    }
   }
 }
