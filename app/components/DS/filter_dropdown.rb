a1727a20c879a7e4bfbab61512de6b4a9ebf516a
Alessio Cappa <alessiocappa26@gmail.com>
2026-02-23 21:52:03 +0100

    feat: add "icon" variant for dropdown

--- app/components/DS/filter_dropdown.rb
+++ app/components/DS/filter_dropdown.rb
@@ -1,7 +1,8 @@
 module DS
   class FilterDropdown < ViewComponent::Base
-    attr_reader :items, :selected, :placeholder, :empty_text,
-                :value_method, :label_method, :label, :searchable, :variant
+    attr_reader :items, :selected_value, :placeholder,
+                :empty_text, :value_method, :label_method,
+                :label, :searchable, :variant
 
     VARIANTS = %i[simple icon badge].freeze
     DEFAULT_COLOR = "#737373"
@@ -15,44 +16,55 @@ module DS
       label_method: :name,
       label: nil,
       searchable: true,
-      variant: :simple
+      variant: :simple,
+      include_blank: nil
     )
-      @items = items
-      @selected = selected
-      @placeholder = placeholder
-      @empty_text = empty_text
       @value_method = value_method
       @label_method = label_method
+      @placeholder = placeholder
+      @empty_text = empty_text
       @label = label
       @searchable = searchable
       @variant = variant.to_sym
 
       raise ArgumentError, "Invalid variant: #{@variant}" unless VARIANTS.include?(@variant)
+
+      normalized_items = normalize_items(items)
+
+      if include_blank
+        normalized_items.unshift({
+          value: nil,
+          label: include_blank,
+          object: nil
+        })
+      end
+
+      @items = normalized_items
+      @selected_value = selected
+    end
+
+    def selected_item
+      items.find { |item| item[:value] == selected_value }
     end
 
     private
 
-    def value_for(item)
-      if item.is_a?(Array) && value_method.is_a?(Integer)
-        item[value_method]
-      elsif item.is_a?(Array)
-        item.last
-      elsif item.is_a?(Hash)
-        item[value_method]
-      else
-        item.public_send(value_method)
-      end
-    end
-
-    def label_for(item)
-      if item.is_a?(Array) && label_method.is_a?(Integer)
-        item[label_method]
-      elsif item.is_a?(Array)
-        item.first
-      elsif item.is_a?(Hash)
-        item[label_method]
-      else
-        item.public_send(label_method)
+    def normalize_items(collection)
+      collection.map do |item|
+        case item
+        when Hash
+          {
+            value: item[:value] || item[value_method],
+            label: item[:label] || item[label_method],
+            object: item[:object]
+          }
+        else
+          {
+            value: item.public_send(value_method),
+            label: item.public_send(label_method),
+            object: item
+          }
+        end
       end
     end
   end
