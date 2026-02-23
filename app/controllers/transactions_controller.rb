136d232b1a31dbb2ecba8ce29fc4bc8c12c8c3bc
bittoby <brianwhitedev1996@gmail.com>
2026-02-23 23:30:22 +0000

    fix: update entry null

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -332,7 +332,7 @@ class TransactionsController < ApplicationController
 
     def resolve_new_merchant!(entry)
       new_name = params[:new_merchant_name].to_s.strip
-      return if new_name.blank?
+      return if new_name.blank? || entry.entryable.nil?
 
       merchant = Current.family.available_merchants.find_by(name: new_name) ||
                  Current.family.merchants.create_or_find_by!(name: new_name)
