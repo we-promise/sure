44a6f4d11f81194b3954de631c536b60326f29d1
Alessio Cappa <alessiocappa26@gmail.com>
2026-02-25 22:58:13 +0100

    fix: selection issue + controller adjustments

--- app/javascript/controllers/form_dropdown_controller.js
+++ app/javascript/controllers/form_dropdown_controller.js
@@ -3,17 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input"]
 
-  connect() {
-    this.element.addEventListener("dropdown:select", (event) => {
-      this.inputTarget.value = event.detail.value
+  onSelect(event) {
+    this.inputTarget.value = event.detail.value
 
-      const changeEvent = new Event("change", { bubbles: true })
-      this.inputTarget.dispatchEvent(changeEvent)
+    const inputEvent = new Event("input", { bubbles: true })
+    this.inputTarget.dispatchEvent(inputEvent)
 
-      const form = this.element.closest("form")
-      if (form && form.dataset.controller?.includes("auto-submit-form")) {
-        form.requestSubmit()
-      }
-    })
+    const form = this.element.closest("form")
+    const controllers = (form?.dataset.controller || "").split(/\s+/)
+    if (form && controllers.includes("auto-submit-form")) {
+      form.requestSubmit()
+    }
   }
 }
