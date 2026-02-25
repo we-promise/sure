584dc7c0091f8453d77f0b77c8397dd5341d7acc
Hugo Dauphinot <hugo.dauph@gmail.com>
2026-02-25 22:13:34 +0100

    add an editable down_payment optionnal field

--- db/migrate/20260225210815_add_down_payment_to_loans.rb
+++ db/migrate/20260225210815_add_down_payment_to_loans.rb
@@ -1,5 +1,5 @@
 class AddDownPaymentToLoans < ActiveRecord::Migration[7.2]
   def change
-    add_column :loans, :down_payment, :decimal
+    add_column :loans, :down_payment, :decimal, precision: 15, scale: 2
   end
 end
