f0d8c8c71b680ea34ccad94c9182b7dd4a3135e2
Hugo Dauphinot <hugo.dauph@gmail.com>
2026-02-25 22:30:21 +0100

    update controller

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -2,6 +2,6 @@ class LoansController < ApplicationController
   include AccountableResource
 
   permitted_accountable_attributes(
-    :id, :subtype, :rate_type, :interest_rate, :term_months, :initial_balance
+    :id, :subtype, :rate_type, :interest_rate, :term_months, :initial_balance, :down_payment
   )
 end
