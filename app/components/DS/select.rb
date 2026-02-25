82caa313dd34d9f73ddf8c42a80f27b2ef74f13c
Alessio Cappa <alessiocappa26@gmail.com>
2026-02-25 21:14:13 +0100

    fix: verify color format

--- app/components/DS/select.rb
+++ app/components/DS/select.rb
@@ -3,6 +3,8 @@ module DS
     attr_reader :form, :method, :items, :selected_value, :placeholder, :variant, :searchable, :options
 
     VARIANTS = %i[simple logo badge].freeze
+    HEX_COLOR_REGEX = /\A#[0-9a-fA-F]{3}(?:[0-9a-fA-F]{3})?\z/
+    RGB_COLOR_REGEX = /\Argb\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*\)\z/
     DEFAULT_COLOR = "#737373"
 
     def initialize(form:, method:, items:, selected: nil, placeholder: I18n.t("helpers.select.default_label"), variant: :simple, include_blank: nil, searchable: false, **options)
@@ -34,7 +36,15 @@ module DS
     # Returns the color for a given item (used in :badge variant)
     def color_for(item)
       obj = item[:object]
-      obj&.respond_to?(:color) ? obj.color : DEFAULT_COLOR
+      color = obj&.respond_to?(:color) ? obj.color : DEFAULT_COLOR
+
+      return DEFAULT_COLOR unless color.is_a?(String)
+
+      if color.match?(HEX_COLOR_REGEX) || color.match?(RGB_COLOR_REGEX)
+        color
+      else
+        DEFAULT_COLOR
+      end
     end
 
     # Returns the lucide_icon name for a given item (used in :badge variant)
