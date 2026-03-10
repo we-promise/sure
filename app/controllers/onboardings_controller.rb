83374ffb5b6f24c5837165b3235b348c7c6bbc3a
Juan José Mata <juanjo.mata@gmail.com>
2026-03-10 20:51:22 +0000

    Skip onboarding steps for `companion`

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -2,7 +2,7 @@ class OnboardingsController < ApplicationController
   layout "wizard"
 
   before_action :set_user
-  before_action :load_invitation
+  before_action :set_joined_existing_family
 
   def show
   end
@@ -18,7 +18,14 @@ class OnboardingsController < ApplicationController
       @user = Current.user
     end
 
-    def load_invitation
+    def set_joined_existing_family
       @invitation = Current.family.invitations.accepted.find_by(email: Current.user.email)
+      @joined_existing_family = @invitation.present? || assigned_to_invite_only_family?
+    end
+
+    def assigned_to_invite_only_family?
+      Setting.onboarding_state == "invite_only" &&
+        Setting.invite_only_default_family_id.present? &&
+        Setting.invite_only_default_family_id == Current.family.id.to_s
     end
 end
