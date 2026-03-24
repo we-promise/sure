746d0ef4b43f7cb20bcd6ccf05d55a1651d0d04e
sokiee <sokysrm@gmail.com>
2026-03-24 09:29:46 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1434,14 +1434,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_24_100001) do
     t.string "external_id"
     t.jsonb "extra", default: {}, null: false
     t.string "investment_activity_label"
-    t.uuid "security_id"
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["external_id"], name: "index_transactions_on_external_id"
     t.index ["extra"], name: "index_transactions_on_extra", using: :gin
     t.index ["investment_activity_label"], name: "index_transactions_on_investment_activity_label"
     t.index ["kind"], name: "index_transactions_on_kind"
     t.index ["merchant_id"], name: "index_transactions_on_merchant_id"
-    t.index ["security_id"], name: "index_transactions_on_security_id"
   end
 
   create_table "transfers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1601,7 +1599,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_24_100001) do
   add_foreign_key "trades", "securities"
   add_foreign_key "transactions", "categories", on_delete: :nullify
   add_foreign_key "transactions", "merchants"
-  add_foreign_key "transactions", "securities"
   add_foreign_key "transfers", "transactions", column: "inflow_transaction_id", on_delete: :cascade
   add_foreign_key "transfers", "transactions", column: "outflow_transaction_id", on_delete: :cascade
   add_foreign_key "users", "accounts", column: "default_account_id", on_delete: :nullify
