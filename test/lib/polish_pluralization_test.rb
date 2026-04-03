6cd72ee3c9633885876f6eba4f9b2a54ee8da57d
UberDudePL <uberdudepl@gmail.com>
2026-04-03 00:15:29 +0000

    Final language improvements and test isolation for Polish locales
    - Improved partial_success wording in SnapTrade with proper noun inflection
    - Fixed typos: Pomin → Pomiń in Mercury and LunchFlow items
    - Isolated I18n backend state in polish_pluralization_test to prevent test coupling

--- test/lib/polish_pluralization_test.rb
+++ test/lib/polish_pluralization_test.rb
@@ -2,21 +2,33 @@ require "test_helper"
 
 class PolishPluralizationTest < ActiveSupport::TestCase
   test "uses rails i18n plural rules for polish" do
-    I18n.backend.store_translations(:pl, test_pluralization: {
-      sample: {
-        one: "one",
-        few: "few",
-        many: "many",
-        other: "other"
-      }
-    })
+    translations = I18n.backend.send(:translations)
+    previous = translations.dig(:pl, :test_pluralization)&.deep_dup
 
-    assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 0)
-    assert_equal "one", I18n.t("test_pluralization.sample", locale: :pl, count: 1)
-    assert_equal "few", I18n.t("test_pluralization.sample", locale: :pl, count: 2)
-    assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 5)
-    assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 12)
-    assert_equal "few", I18n.t("test_pluralization.sample", locale: :pl, count: 22)
-    assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 25)
+    begin
+      I18n.backend.store_translations(:pl, test_pluralization: {
+        sample: {
+          one: "one",
+          few: "few",
+          many: "many",
+          other: "other"
+        }
+      })
+
+      assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 0)
+      assert_equal "one", I18n.t("test_pluralization.sample", locale: :pl, count: 1)
+      assert_equal "few", I18n.t("test_pluralization.sample", locale: :pl, count: 2)
+      assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 5)
+      assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 12)
+      assert_equal "few", I18n.t("test_pluralization.sample", locale: :pl, count: 22)
+      assert_equal "many", I18n.t("test_pluralization.sample", locale: :pl, count: 25)
+    ensure
+      if previous.nil?
+        translations[:pl]&.delete(:test_pluralization)
+      else
+        translations[:pl] ||= {}
+        translations[:pl][:test_pluralization] = previous
+      end
+    end
   end
 end
