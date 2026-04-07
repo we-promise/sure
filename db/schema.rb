ff45dcc2d4a5e0b3b9679525593457e55576ae00
dev-miro26 <devmiro26@gmail.com>
2026-04-07 14:58:50 -0400

    Fix typo in schema.rb by removing extraneous character from pgcrypto extension enablement

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.2].define(version: 2026_03_30_050801) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"s
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
