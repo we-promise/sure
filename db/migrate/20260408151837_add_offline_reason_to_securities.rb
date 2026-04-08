29f28d9c31665e36728c4243b2b6d84e2645c187
sokiee <sokysrm@gmail.com>
2026-04-08 18:10:37 +0200

    Update 20260408151837_add_offline_reason_to_securities.rb

--- db/migrate/20260408151837_add_offline_reason_to_securities.rb
+++ db/migrate/20260408151837_add_offline_reason_to_securities.rb
@@ -3,6 +3,6 @@ class AddOfflineReasonToSecurities < ActiveRecord::Migration[7.2]
 
   def change
     add_column :securities, :offline_reason, :string
-    add_index :securities, [:price_provider, :offline_reason], algorithm: :concurrently
+    add_index :securities, [ :price_provider, :offline_reason ], algorithm: :concurrently
   end
 end
