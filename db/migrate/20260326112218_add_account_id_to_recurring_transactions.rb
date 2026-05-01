6d2a6f86c6db9f76853afe0c13a5f9533a9be667
SureBot <sure-bot@we-promise.com>
2026-05-01 20:20:23 +0000

    style(recurring): move helper below public migration methods

--- db/migrate/20260326112218_add_account_id_to_recurring_transactions.rb
+++ db/migrate/20260326112218_add_account_id_to_recurring_transactions.rb
@@ -69,6 +69,25 @@ class AddAccountIdToRecurringTransactions < ActiveRecord::Migration[7.2]
       name: "idx_recurring_txns_acct_name"
   end
 
+  def down
+    remove_index :recurring_transactions, name: "idx_recurring_txns_acct_merchant", if_exists: true
+    remove_index :recurring_transactions, name: "idx_recurring_txns_acct_name", if_exists: true
+
+    add_index :recurring_transactions,
+      [ :family_id, :merchant_id, :amount, :currency ],
+      unique: true,
+      where: "merchant_id IS NOT NULL",
+      name: "idx_recurring_txns_merchant"
+
+    add_index :recurring_transactions,
+      [ :family_id, :name, :amount, :currency ],
+      unique: true,
+      where: "name IS NOT NULL AND merchant_id IS NULL",
+      name: "idx_recurring_txns_name"
+
+    remove_reference :recurring_transactions, :account
+  end
+
   private
 
     def dedupe_recurring_transactions!(partition_columns:, where_clause:)
@@ -92,23 +111,4 @@ class AddAccountIdToRecurringTransactions < ActiveRecord::Migration[7.2]
         )
       SQL
     end
-
-  def down
-    remove_index :recurring_transactions, name: "idx_recurring_txns_acct_merchant", if_exists: true
-    remove_index :recurring_transactions, name: "idx_recurring_txns_acct_name", if_exists: true
-
-    add_index :recurring_transactions,
-      [ :family_id, :merchant_id, :amount, :currency ],
-      unique: true,
-      where: "merchant_id IS NOT NULL",
-      name: "idx_recurring_txns_merchant"
-
-    add_index :recurring_transactions,
-      [ :family_id, :name, :amount, :currency ],
-      unique: true,
-      where: "name IS NOT NULL AND merchant_id IS NULL",
-      name: "idx_recurring_txns_name"
-
-    remove_reference :recurring_transactions, :account
-  end
 end
