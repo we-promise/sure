ed598417d3507f8dad6dd60a3c94bfc51d235827
SureBot <sure-bot@we-promise.com>
2026-05-04 03:32:46 +0000

    test: capture provider histories across tool calls

--- test/models/assistant_test.rb
+++ test/models/assistant_test.rb
@@ -192,11 +192,20 @@ class AssistantTest < ActiveSupport::TestCase
     ]
 
     sequence = sequence("provider_chat_response")
+    seen_histories = []
 
     @provider.expects(:chat_response).with do |message, **options|
       assert_equal @expected_session_id, options[:session_id]
       assert_equal @expected_user_identifier, options[:user_identifier]
-      assert_equal expected_follow_up_history, options[:messages]
+      seen_histories << options[:messages]
+      options[:streamer].call(call1_response_chunk)
+      true
+    end.returns(call1_response).once.in_sequence(sequence)
+
+    @provider.expects(:chat_response).with do |message, **options|
+      assert_equal @expected_session_id, options[:session_id]
+      assert_equal @expected_user_identifier, options[:user_identifier]
+      seen_histories << options[:messages]
       call2_text_chunks.each do |text_chunk|
         options[:streamer].call(text_chunk)
       end
@@ -205,19 +214,12 @@ class AssistantTest < ActiveSupport::TestCase
       true
     end.returns(call2_response).once.in_sequence(sequence)
 
-    @provider.expects(:chat_response).with do |message, **options|
-      assert_equal @expected_session_id, options[:session_id]
-      assert_equal @expected_user_identifier, options[:user_identifier]
-      assert_equal @expected_conversation_history, options[:messages]
-      options[:streamer].call(call1_response_chunk)
-      true
-    end.returns(call1_response).once.in_sequence(sequence)
-
     assert_no_difference "AssistantMessage.count" do
       @assistant.respond_to(@message, assistant_message: assistant_message)
       message = assistant_message.reload
       assert_equal 1, message.tool_calls.size
       assert analyzing_state_seen_during_tool_call
+      assert_equal [@expected_conversation_history, expected_follow_up_history], seen_histories
     end
   end
 
