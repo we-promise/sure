8c6de7b84355256f7f27004909e37dd790567708
oxtensor <me@oxtensor.com>
2026-05-14 12:14:06 +0000

    fix(a11y): extend skip-link and #main anchor to settings layout

--- test/integration/layout_accessibility_test.rb
+++ test/integration/layout_accessibility_test.rb
@@ -14,4 +14,14 @@ class LayoutAccessibilityTest < ActionDispatch::IntegrationTest
     assert_select "a[href=\"#main\"]", text: skip_text
     assert_select "main#main"
   end
+
+  test "settings layout renders skip-link pointing at #main and a <main> with id=\"main\"" do
+    get settings_profile_path
+    assert_response :ok
+
+    skip_text = I18n.t("layouts.application.skip_to_main")
+
+    assert_select "a[href=\"#main\"]", text: skip_text
+    assert_select "main#main"
+  end
 end
