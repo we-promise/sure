17645b2733bc5a71a93acc791f300011835de2d6
sure-admin <sure-admin@splashblot.com>
2026-05-17 14:18:43 +0000

    Make debug log retention configurable

--- app/jobs/debug_log_cleanup_job.rb
+++ app/jobs/debug_log_cleanup_job.rb
@@ -3,10 +3,13 @@
 class DebugLogCleanupJob < ApplicationJob
   queue_as :scheduled
 
-  RETENTION_PERIOD = 90.days
-
   def perform
-    deleted_count = DebugLogEntry.where(created_at: ...RETENTION_PERIOD.ago).delete_all
-    Rails.logger.info("DebugLogCleanupJob: Deleted #{deleted_count} debug log entries older than #{RETENTION_PERIOD.inspect}") if deleted_count > 0
+    deleted_count = DebugLogEntry.where(created_at: ...retention_period.ago).delete_all
+    Rails.logger.info("DebugLogCleanupJob: Deleted #{deleted_count} debug log entries older than #{retention_period.inspect}") if deleted_count > 0
   end
+
+  private
+    def retention_period
+      Rails.application.config.x.debug_log.retention_days.days
+    end
 end
