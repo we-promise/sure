80596d538e5890df50d1f45e4b925223a3a2c46f
glorysr1209-png <glorysr1209@gmail.com>
2026-06-01 08:25:56 -0400

    fix: scope bulk transaction updates to writable accounts

--- app/controllers/transactions/bulk_updates_controller.rb
+++ app/controllers/transactions/bulk_updates_controller.rb
@@ -3,12 +3,19 @@ class Transactions::BulkUpdatesController < ApplicationController
   end
 
   def create
+    writable_account_ids = Current.family.accounts.writable_by(Current.user).pluck(:id)
+    requested_ids = Array(bulk_update_params[:entry_ids]).map(&:to_i)
     # Skip split parents from bulk update - update children instead
     updated = Current.family
                      .entries
                      .excluding_split_parents
-                     .where(id: bulk_update_params[:entry_ids])
+                     .where(account_id: writable_account_ids, id: requested_ids)
                      .bulk_update!(bulk_update_params, update_tags: tags_provided?)
+
+    if updated < requested_ids.length
+      redirect_back_or_to transactions_path, alert: "Some transactions could not be updated due to permissions"
+      return
+    end
 
     redirect_back_or_to transactions_path, notice: "#{updated} transactions updated"
   end
