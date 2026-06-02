c4dcb0585a54b212423e42108dd854dff5853013
dale053 <star05223@outlook.com>
2026-06-02 06:24:01 -0400

    fix(schema): remove duplicate define block causing syntax error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2026_06_01_000000) do
-ActiveRecord::Schema[7.2].define(version: 2026_05_31_153000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
