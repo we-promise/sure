fabb7fc44d7b0658d7234742f8a2bdba331f0d4b
julien gourmet <julien.gourmet@epita.fr>
2026-06-06 15:27:13 +0200

    feat(schema): update schema version and remove account_providers_count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_31_213000) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_05_100000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -118,7 +118,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_31_213000) do
     t.text "notes"
     t.uuid "owner_id"
     t.datetime "disabled_at"
-    t.integer "account_providers_count", default: 0, null: false
     t.index ["accountable_id", "accountable_type"], name: "index_accounts_on_accountable_id_and_accountable_type"
     t.index ["accountable_type"], name: "index_accounts_on_accountable_type"
     t.index ["currency"], name: "index_accounts_on_currency"
