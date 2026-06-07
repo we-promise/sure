e524859be34cfb029ffbf980e94f67b489bb6aa0
JSONbored <49853598+JSONbored@users.noreply.github.com>
2026-06-07 03:27:37 -0700

    fix(sync): make transfer lookup index migration reversible

--- db/migrate/20260607071000_tighten_transfer_match_lookup_index.rb
+++ db/migrate/20260607071000_tighten_transfer_match_lookup_index.rb
@@ -1,7 +1,7 @@
 class TightenTransferMatchLookupIndex < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
 
-  def change
+  def up
     remove_index :entries,
                  name: "index_entries_on_transfer_match_lookup",
                  if_exists: true,
@@ -11,6 +11,19 @@ class TightenTransferMatchLookupIndex < ActiveRecord::Migration[7.2]
               [ :currency, :amount, :date, :account_id ],
               name: "index_entries_on_transfer_match_lookup",
               where: "entryable_type = 'Transaction' AND excluded = false",
+              algorithm: :concurrently
+  end
+
+  def down
+    remove_index :entries,
+                 name: "index_entries_on_transfer_match_lookup",
+                 if_exists: true,
+                 algorithm: :concurrently
+
+    add_index :entries,
+              [ :currency, :amount, :date, :account_id ],
+              name: "index_entries_on_transfer_match_lookup",
+              where: "entryable_type = 'Transaction'",
               algorithm: :concurrently
   end
 end
