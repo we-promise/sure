9d0836c2cb53bb8ddec97a98f251368e8e1fdf80
kmshdev <me@kmsh.dev>
2026-06-11 17:58:45 +0530

    Add tax workbook import system test

---
+++ test/system/tax_workbook_imports_test.rb
@@ -0,0 +1,32 @@
+require "application_system_test_case"
+
+class TaxWorkbookImportsTest < ApplicationSystemTestCase
+  setup do
+    Rails.configuration.stubs(:app_mode).returns("managed".inquiry)
+    ensure_tailwind_build
+    sign_in users(:family_admin)
+
+    @fixture_path = Rails.root.join("tmp", "tax_workbook_system_test.xlsx")
+    File.binwrite(@fixture_path, TaxWorkbook::TemplateGenerator.new.call)
+  end
+
+  teardown do
+    File.delete(@fixture_path) if File.exist?(@fixture_path)
+  end
+
+  test "uploads and searches tax workbook records" do
+    visit tax_workbook_imports_path
+
+    attach_file "tax_workbook_import_file", @fixture_path
+    click_button I18n.t("tax_workbook_imports.index.upload_button")
+
+    assert_text I18n.t("tax_workbook_imports.create.success")
+    assert_text "Risingstone infra pvt ltd"
+
+    visit tax_records_path
+    fill_in I18n.t("tax_records.index.search_label"), with: "INV-001"
+    find("input[name='q[search]']").send_keys(:enter)
+
+    assert_text "INV-001"
+  end
+end
