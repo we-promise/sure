102af3ab05d7814ea47a0a68cfa5c3e918c9db19
xBlaz3kx <blaz.dular@gmail.com>
2026-06-17 23:53:50 +0200

    fix(migration): convert change to up/down for reversibility

--- db/migrate/20260602120000_add_null_constraints_to_bitstamp_tables.rb
+++ db/migrate/20260602120000_add_null_constraints_to_bitstamp_tables.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class AddNullConstraintsToBitstampTables < ActiveRecord::Migration[7.2]
-  def change
+  def up
     execute "UPDATE bitstamp_items SET name = 'Bitstamp' WHERE name IS NULL"
     execute "UPDATE bitstamp_accounts SET name = 'Unknown' WHERE name IS NULL"
     execute "UPDATE bitstamp_accounts SET account_type = 'unknown' WHERE account_type IS NULL"
+
     null_currency_count = select_value("SELECT COUNT(*) FROM bitstamp_accounts WHERE currency IS NULL").to_i
     if null_currency_count > 0
       raise "Cannot add NOT NULL constraint to bitstamp_accounts.currency: " \
@@ -16,5 +17,12 @@ class AddNullConstraintsToBitstampTables < ActiveRecord::Migration[7.2]
     change_column_null :bitstamp_accounts, :name, false
     change_column_null :bitstamp_accounts, :account_type, false
     change_column_null :bitstamp_accounts, :currency, false
+  end
+
+  def down
+    change_column_null :bitstamp_items, :name, true
+    change_column_null :bitstamp_accounts, :name, true
+    change_column_null :bitstamp_accounts, :account_type, true
+    change_column_null :bitstamp_accounts, :currency, true
   end
 end
