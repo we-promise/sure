a927ab8d0639eeb1621d5a78507bd32d1d498162
Juan José Mata <jjmata@jjmata.com>
2026-06-26 01:16:08 +0200

    Add SnapTrade OAuth device flow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_17_120000) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_25_230639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -1752,6 +1752,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_17_120000) do
     t.string "consumer_key"
     t.string "snaptrade_user_id"
     t.string "snaptrade_user_secret"
+    t.string "oauth_access_token"
+    t.string "oauth_refresh_token"
+    t.string "oauth_token_type"
+    t.string "oauth_scope"
+    t.datetime "oauth_token_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["family_id"], name: "index_snaptrade_items_on_family_id"
