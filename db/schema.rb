76fe10798d5c0d57a753a40ca194fefe6d59c15e
DataEnginr <dataenginr@example.com>
2026-06-28 19:53:02 +0000

    Update schema.rb version after migrate, preserve clean diff vs main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_25_230639) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_28_200000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
