187fed55172e97fb5ceb103c068acf348324e401
Vidhu Arora <TheRodzz@users.noreply.github.com>
2026-07-31 23:16:53 +0530

    perf: fix N+1 in TransactionsController#index on transfer counterparty lookups
    Resolves #2818
    
    Fully preloads both inflow_transaction and outflow_transaction (with their entries and accounts) for transfer_as_outflow and transfer_as_inflow to prevent N+1 queries when Transfer#to_account is called by categories/menu.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -22,8 +22,8 @@ class TransactionsController < ApplicationController
                        .includes(
                           { entry: :account },
                           :category, :merchant, :tags,
-                          transfer_as_outflow: { inflow_transaction: { entry: :account } },
-                          transfer_as_inflow: { outflow_transaction: { entry: :account } }
+                          transfer_as_outflow: { inflow_transaction: { entry: :account }, outflow_transaction: { entry: :account } },
+                          transfer_as_inflow: { outflow_transaction: { entry: :account }, inflow_transaction: { entry: :account } }
                         )
 
     @pagy, @transactions = pagy(base_scope, limit: safe_per_page)
