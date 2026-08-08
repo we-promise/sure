3402c28ce2bdc29f7de75aeea368dd8543270fe3
atlas-maxjb <atlas@maximusjb.com>
2026-08-08 00:13:01 -0400

    Backfill cash-equivalent holdings

--- db/migrate/20260803000000_add_cash_equivalent_to_holdings.rb
+++ db/migrate/20260803000000_add_cash_equivalent_to_holdings.rb
@@ -1,5 +1,13 @@
 class AddCashEquivalentToHoldings < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :holdings, :cash_equivalent, :boolean, default: false, null: false
+
+    say_with_time "Backfilling cash-equivalent holdings from stored provider snapshots" do
+      Holding::CashEquivalentBackfill.run.total_holdings
+    end
+  end
+
+  def down
+    remove_column :holdings, :cash_equivalent
   end
 end
