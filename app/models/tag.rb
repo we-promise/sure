5326c77b6e832858b735a6ce9b3d6ac003730e65
julien gourmet <julien.gourmet@epita.fr>
2026-08-09 15:44:43 +0200

    feat(tags): ensure pockets are destroyed when their associated tag is deleted

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -15,7 +15,7 @@ class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :transactions, through: :taggings, source: :taggable, source_type: "Transaction"
   has_many :import_mappings, as: :mappable, dependent: :destroy, class_name: "Import::Mapping"
-  has_many :pockets, dependent: :nullify
+  has_many :pockets, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :family }
   validates :name, exclusion: { in: [ UNTAGGED_FILTER_VALUE ] }
