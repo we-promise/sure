5d3c3d9cd28972949294cb408b31c43c0fa56b0a
atlas-maxjb <atlas@maximusjb.com>
2026-08-14 14:55:03 -0400

    Fix transaction imports for connected accounts

--- test/controllers/import/mappings_controller_test.rb
+++ test/controllers/import/mappings_controller_test.rb
@@ -26,4 +26,24 @@ class Import::MappingsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to import_confirm_path(@import)
   end
+
+  test "account mapping lists connected accounts as targets" do
+    @import.update!(
+      raw_file_str: <<~CSV,
+        date,amount,account
+        #{Date.current.iso8601},25,Plaid Depository Account
+      CSV
+      date_col_label: "date",
+      amount_col_label: "amount",
+      account_col_label: "account",
+      date_format: "%Y-%m-%d"
+    )
+    @import.generate_rows_from_csv
+    @import.sync_mappings
+
+    get import_confirm_path(@import)
+
+    assert_response :success
+    assert_select "select option", text: "Plaid Depository Account"
+  end
 end
