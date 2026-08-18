01069c7efb5183b029eedfb94e701329c49745cc
mr-u0b0dy <63730630+mr-u0b0dy@users.noreply.github.com>
2026-08-18 23:26:15 +0530

    fix: update badge query to target DS::Pill structure

--- app/javascript/controllers/category_badge_select_controller.js
+++ app/javascript/controllers/category_badge_select_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
     );
     if (!option) return;
 
-    const badge = option.querySelector("span.flex.items-center");
+    const badge = option.querySelector("span.inline-flex.items-center");
     if (badge) {
       this.buttonTarget.innerHTML = badge.outerHTML;
     } else {
