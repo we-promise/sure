4b69bf493ab3e8fb7e2efa2bc98b4931c3d8e6df
sure-admin <sure-admin@splashblot.com>
2026-08-26 05:48:58 +0000

    Standardize provider encryption warnings

--- test/controllers/settings/providers_controller_test.rb
+++ test/controllers/settings/providers_controller_test.rb
@@ -495,18 +495,17 @@ class Settings::ProvidersControllerTest < ActionDispatch::IntegrationTest
     refute_includes response.body, I18n.t("settings.providers.drawer_trust_statement_encryption_unconfigured")
   end
 
-  test "GET connect_form does not duplicate provider-specific encryption warnings" do
+  test "GET connect_form uses shared encryption warning for provider panels" do
     Rails.configuration.stubs(:app_mode).returns("self_hosted".inquiry)
     ActiveRecordEncryptionConfig.stubs(:explicitly_configured?).returns(false)
-    WiseItem.stubs(:encryption_ready?).returns(false)
 
     get connect_form_settings_providers_path(provider_key: "wise")
 
     assert_response :success
-    assert_includes response.body, I18n.t("wise_items.provider_panel.encryption_warning.title")
-    assert_includes response.body, I18n.t("wise_items.provider_panel.encryption_warning.message")
-    refute_includes response.body, I18n.t("settings.providers.provider_setup_encryption_warning.title")
-    refute_includes response.body, I18n.t("settings.providers.provider_setup_encryption_warning.message")
+    assert_includes response.body, I18n.t("settings.providers.provider_setup_encryption_warning.title")
+    assert_includes response.body, I18n.t("settings.providers.provider_setup_encryption_warning.message")
+    refute_includes response.body, I18n.t("wise_items.provider_panel.encryption_warning.title")
+    refute_includes response.body, I18n.t("wise_items.provider_panel.encryption_warning.message")
     assert_includes response.body, I18n.t("settings.providers.drawer_trust_statement_encryption_unconfigured")
     refute_includes response.body, I18n.t("settings.providers.drawer_trust_statement")
   end
