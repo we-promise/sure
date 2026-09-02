6dd61e6533e913893a7ac17c723de61bec60bea3
Jonathan Kaiser <jaysbeekay@users.noreply.github.com>
2026-09-02 09:01:22 +0200

    chore(loans): remove obsolete recurring_transactions index migration (unrelated to loans feature)

--- db/migrate/20260519191902_remove_obsolete_recurring_transactions_unique_index.rb
+++
@@ -1,7 +0,0 @@
-class RemoveObsoleteRecurringTransactionsUniqueIndex < ActiveRecord::Migration[7.2]
-  def change
-    remove_index :recurring_transactions,
-      name: "idx_recurring_txns_on_family_merchant_amount_currency",
-      if_exists: true
-  end
-end
