672cbe95ed96f97cadb2f9ffe6830e90cb1251dc
Michal Tajchert <mtajchert@gmail.com>
2026-09-07 15:05:37 +0200

    Isolate Wise SCA localization success from key generation

--- test/controllers/wise_sca_localization_test.rb
+++ test/controllers/wise_sca_localization_test.rb
@@ -69,6 +69,8 @@ class WiseScaPanelLocalizationTest < ActionDispatch::IntegrationTest
   end
 
   test "German Wise SCA keypair generation returns the localized success message" do
+    WiseItem.any_instance.expects(:generate_sca_keypair!).once
+
     post generate_sca_keypair_wise_item_url(wise_items(:one))
 
     assert_redirected_to accounts_path
