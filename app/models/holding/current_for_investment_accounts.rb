72de725b122e8c3f391e4bb87fff415d44773c9f
drink970082 <howdywu@gmail.com>
2026-09-10 19:54:44 -0400

    Use static SQL for current holdings query

--- app/models/holding/current_for_investment_accounts.rb
+++ app/models/holding/current_for_investment_accounts.rb
@@ -1,4 +1,37 @@
 class Holding::CurrentForInvestmentAccounts
+  CURRENT_HOLDINGS_SQL = <<~SQL.squish.freeze
+    (
+      holdings.account_provider_id IS NOT NULL
+      AND holdings.updated_at::date = (
+        SELECT MAX(provider_holdings.updated_at::date)
+        FROM holdings provider_holdings
+        WHERE provider_holdings.account_id = holdings.account_id
+          AND provider_holdings.account_provider_id IS NOT NULL
+      )
+    ) OR (
+      NOT EXISTS (
+        SELECT 1
+        FROM holdings provider_holdings
+        WHERE provider_holdings.account_id = holdings.account_id
+          AND provider_holdings.account_provider_id IS NOT NULL
+      )
+      AND holdings.currency = (
+        SELECT accounts.currency
+        FROM accounts
+        WHERE accounts.id = holdings.account_id
+      )
+      AND holdings.id = (
+        SELECT latest_holdings.id
+        FROM holdings latest_holdings
+        WHERE latest_holdings.account_id = holdings.account_id
+          AND latest_holdings.security_id = holdings.security_id
+          AND latest_holdings.currency = holdings.currency
+        ORDER BY latest_holdings.date DESC
+        LIMIT 1
+      )
+    )
+  SQL
+
   def initialize(account_ids)
     @account_ids = account_ids
   end
@@ -9,46 +42,9 @@ class Holding::CurrentForInvestmentAccounts
     Holding
       .where(account_id: account_ids)
       .where.not(qty: 0)
-      .where(Arel.sql("(#{provider_import_day}) OR (#{manual_holdings})"))
+      .where(Arel.sql(CURRENT_HOLDINGS_SQL))
   end
 
   private
     attr_reader :account_ids
-
-    def provider_import_day
-      <<~SQL.squish
-        holdings.account_provider_id IS NOT NULL
-        AND holdings.updated_at::date = (
-          SELECT MAX(provider_holdings.updated_at::date)
-          FROM holdings provider_holdings
-          WHERE provider_holdings.account_id = holdings.account_id
-            AND provider_holdings.account_provider_id IS NOT NULL
-        )
-      SQL
-    end
-
-    def manual_holdings
-      <<~SQL.squish
-        NOT EXISTS (
-          SELECT 1
-          FROM holdings provider_holdings
-          WHERE provider_holdings.account_id = holdings.account_id
-            AND provider_holdings.account_provider_id IS NOT NULL
-        )
-        AND holdings.currency = (
-          SELECT accounts.currency
-          FROM accounts
-          WHERE accounts.id = holdings.account_id
-        )
-        AND holdings.id = (
-          SELECT latest_holdings.id
-          FROM holdings latest_holdings
-          WHERE latest_holdings.account_id = holdings.account_id
-            AND latest_holdings.security_id = holdings.security_id
-            AND latest_holdings.currency = holdings.currency
-          ORDER BY latest_holdings.date DESC
-          LIMIT 1
-        )
-      SQL
-    end
 end
