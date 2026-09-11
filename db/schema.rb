bbc42af52eb698bc1e6bbdb6aaf3617aaee98fa5
jaysbeekay <46616837+jaysbeekay@users.noreply.github.com>
2026-09-11 02:58:49 +0200

    Fix Coinbase cryptocurrency holding precision (#3456)
    * Fix Coinbase holding precision
    
    * Use project migration compatibility version
    
    * Complete Coinbase precision remediation
    
    * Preserve Coinbase precision headroom
    
    ---------
    
    Signed-off-by: Juan José Mata <juanjo.mata@gmail.com>
    Co-authored-by: Juan José Mata <juanjo.mata@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -435,7 +435,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_130000) do
     t.uuid "coinbase_item_id", null: false
     t.datetime "created_at", null: false
     t.string "currency"
-    t.decimal "current_balance", precision: 19, scale: 4
+    t.decimal "current_balance", precision: 34, scale: 18
     t.jsonb "institution_metadata"
     t.string "name"
     t.string "provider"
@@ -916,7 +916,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_130000) do
     t.string "external_id"
     t.decimal "price", precision: 19, scale: 4, null: false
     t.uuid "provider_security_id"
-    t.decimal "qty", precision: 24, scale: 8, null: false
+    t.decimal "qty", precision: 34, scale: 18, null: false
     t.uuid "security_id", null: false
     t.boolean "security_locked", default: false, null: false
     t.datetime "updated_at", null: false
@@ -2393,7 +2393,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_130000) do
     t.string "investment_activity_label"
     t.jsonb "locked_attributes", default: {}
     t.decimal "price", precision: 19, scale: 10
-    t.decimal "qty", precision: 24, scale: 8
+    t.decimal "qty", precision: 34, scale: 18
     t.uuid "security_id", null: false
     t.datetime "updated_at", null: false
     t.index ["extra"], name: "index_trades_on_extra", using: :gin
